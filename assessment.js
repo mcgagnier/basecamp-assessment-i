b4f4593a6ba9b88d8296a06467f641d84bc04d3e
Chris <chris@users-mbp.lan>
2018-01-05 07:07:17 -0700

    test

--- assessment.js
+++ assessment.js
@@ -25,7 +25,6 @@ var notHakeem = myName !='Hakeem Olajuwon';
 // #7 Create an if statement that checks the age variable below. If age is greater than 16, then set canDrive to true otherwise set canDrive to false. The canDrive variable will be set by the unit test so do not adjust it.
 var age = 18;
 var canDrive;
-
 if (age > 16) {
   canDrive = true;
 }
